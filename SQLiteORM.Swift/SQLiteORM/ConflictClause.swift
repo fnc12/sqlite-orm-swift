e4d93d5d5bfc9818e2aa58cca2f7eb97ba45c360
Yevgeniy Zakharov <fnc12@me.com>
2021-09-13 10:21:21 +0500

    removed dups

--- SQLiteORM.Swift/SQLiteORM/ConflictClause.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-public enum ConflictClause {
-    case rollback
-    case abort
-    case fail
-    case ignore
-    case replace
-}
--- SQLiteORM.Swift/SQLiteORM/Order.swift
+++
@@ -1,6 +0,0 @@
-import Foundation
-
-public enum Order {
-    case asc
-    case desc
-}
