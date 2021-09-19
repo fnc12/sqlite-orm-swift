0ba8e6ec7bbbc01f4ea3d91c539a2f87b3a0c7ee
Yevgeniy Zakharov <fnc12@me.com>
2021-09-19 14:54:31 +0500

    fixed CI error

--- Tests/SQLiteORMTests/mocks/BinderMock.swift
+++ Tests/SQLiteORMTests/mocks/BinderMock.swift
@@ -1,38 +1,38 @@
 import Foundation
 @testable import SQLiteORM
 
-class BinderMock: Mock<CallType> {
-    enum CallType: Equatable {
-        case bindInt(value: Int)
-        case bindDouble(value: Double)
-        case bindText(value: String)
-        case bindNull
-        
-        static func ==(lhs: Self, rhs: Self) -> Bool {
-            switch (lhs, rhs) {
-            case let (.bindInt(leftValue), .bindInt(rightValue)):
-                return leftValue == rightValue
-            case let (.bindDouble(leftValue), .bindDouble(rightValue)):
-                return leftValue == rightValue
-            case let (.bindText(leftValue), .bindText(rightValue)):
-                return leftValue == rightValue
-            case (.bindNull, .bindNull):
-                return true
-            default:
-                return false
-            }
-        }
-    }
-    
-    typealias Call = MockCall<CallType>
+class BinderMock: Mock<BinderCallType> {
+    typealias Call = MockCall<BinderCallType>
     
     var nextCallId = 0
     var calls = [Call]()
     
-    private func makeCall(with callType: CallType) -> Call {
+    private func makeCall(with callType: BinderCallType) -> Call {
         let res = MockCall(id: self.nextCallId, callType: callType)
         self.nextCallId += 1
         return res
+    }
+}
+
+enum BinderCallType: Equatable {
+    case bindInt(value: Int)
+    case bindDouble(value: Double)
+    case bindText(value: String)
+    case bindNull
+    
+    static func ==(lhs: Self, rhs: Self) -> Bool {
+        switch (lhs, rhs) {
+        case let (.bindInt(leftValue), .bindInt(rightValue)):
+            return leftValue == rightValue
+        case let (.bindDouble(leftValue), .bindDouble(rightValue)):
+            return leftValue == rightValue
+        case let (.bindText(leftValue), .bindText(rightValue)):
+            return leftValue == rightValue
+        case (.bindNull, .bindNull):
+            return true
+        default:
+            return false
+        }
     }
 }
 
