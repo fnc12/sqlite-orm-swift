116214d18dd1b8130c556042ed5cc5f926954e44
Yevgeniy Zakharov <fnc12@me.com>
2022-09-11 20:01:15 +0100

    tests refactored

---
+++ Tests/SQLiteORMTests/StorageCoreTests/StorageCoreSchemaTests.swift
@@ -0,0 +1,90 @@
+import XCTest
+@testable import SQLiteORM
+
+class StorageCoreSchemaTests: XCTestCase {
+    struct User: Initializable, Equatable {
+        var id = 0
+        var name = ""
+
+        static func == (lhs: Self, rhs: Self) -> Bool {
+            return lhs.id == rhs.id && lhs.name == rhs.name
+        }
+    }
+    
+    func testTableExists() throws {
+        let apiProvider = SQLiteApiProviderMock()
+        apiProvider.forwardsCalls = true
+        let storageCore = try StorageCoreImpl(filename: "",
+                                              apiProvider: apiProvider,
+                                              tables: [Table<User>(name: "users",
+                                                                   columns:
+                                                                    Column(name: "id", keyPath: \User.id, constraints: primaryKey(), notNull()),
+                                                                   Column(name: "name", keyPath: \User.name, constraints: notNull()))])
+        apiProvider.resetCalls()
+        switch storageCore.tableExists(with: "users") {
+        case .success(let value):
+            XCTAssertFalse(value)
+        case .failure(let error):
+            throw error
+        }
+        XCTAssertEqual(apiProvider.calls, [
+            .init(id: 0, callType: .sqlite3PrepareV2(.ignore, "SELECT COUNT(*) FROM sqlite_master WHERE type = \'table\' AND name = \'users\'", -1, .ignore, nil)),
+            .init(id: 1, callType: .sqlite3Step(.ignore)),
+            .init(id: 2, callType: .sqlite3ColumnInt(.ignore, 0)),
+            .init(id: 3, callType: .sqlite3Step(.ignore)),
+            .init(id: 4, callType: .sqlite3Finalize(.ignore))
+        ])
+
+        apiProvider.resetCalls()
+        switch storageCore.tableExists(with: "visits") {
+        case .success(let value):
+            XCTAssertFalse(value)
+        case .failure(let error):
+            throw error
+        }
+        XCTAssertEqual(apiProvider.calls, [
+            .init(id: 0, callType: .sqlite3PrepareV2(.ignore, "SELECT COUNT(*) FROM sqlite_master WHERE type = \'table\' AND name = \'visits\'", -1, .ignore, nil)),
+            .init(id: 1, callType: .sqlite3Step(.ignore)),
+            .init(id: 2, callType: .sqlite3ColumnInt(.ignore, 0)),
+            .init(id: 3, callType: .sqlite3Step(.ignore)),
+            .init(id: 4, callType: .sqlite3Finalize(.ignore))
+        ])
+
+        switch storageCore.syncSchema(preserve: true) {
+        case .success(_):
+            break
+        case .failure(let error):
+            throw error
+        }
+
+        apiProvider.resetCalls()
+        switch storageCore.tableExists(with: "users") {
+        case .success(let value):
+            XCTAssertTrue(value)
+        case .failure(let error):
+            throw error
+        }
+        XCTAssertEqual(apiProvider.calls, [
+            .init(id: 0, callType: .sqlite3PrepareV2(.ignore, "SELECT COUNT(*) FROM sqlite_master WHERE type = \'table\' AND name = \'users\'", -1, .ignore, nil)),
+            .init(id: 1, callType: .sqlite3Step(.ignore)),
+            .init(id: 2, callType: .sqlite3ColumnInt(.ignore, 0)),
+            .init(id: 3, callType: .sqlite3Step(.ignore)),
+            .init(id: 4, callType: .sqlite3Finalize(.ignore))
+        ])
+
+        apiProvider.resetCalls()
+        switch storageCore.tableExists(with: "visits") {
+        case .success(let value):
+            XCTAssertFalse(value)
+        case .failure(let error):
+            throw error
+        }
+        XCTAssertEqual(apiProvider.calls, [
+            .init(id: 0, callType: .sqlite3PrepareV2(.ignore, "SELECT COUNT(*) FROM sqlite_master WHERE type = \'table\' AND name = \'visits\'", -1, .ignore, nil)),
+            .init(id: 1, callType: .sqlite3Step(.ignore)),
+            .init(id: 2, callType: .sqlite3ColumnInt(.ignore, 0)),
+            .init(id: 3, callType: .sqlite3Step(.ignore)),
+            .init(id: 4, callType: .sqlite3Finalize(.ignore))
+        ])
+    }
+}
---
+++ Tests/SQLiteORMTests/StorageCoreTests/StorageCoreTests.swift
@@ -0,0 +1,78 @@
+import XCTest
+@testable import SQLiteORM
+
+class StorageCoreTests: XCTestCase {
+    func testFilename() throws {
+        struct TestCase {
+            let filename: String
+        }
+        let testCases = [
+            TestCase(filename: "ototo"),
+            TestCase(filename: ""),
+            TestCase(filename: ":memory:"),
+            TestCase(filename: "db.sqlite"),
+            TestCase(filename: "company.db")
+        ]
+        for testCase in testCases {
+            let apiProvider = SQLiteApiProviderMock()
+            apiProvider.forwardsCalls = true
+            let storageCore = try StorageCoreImpl(filename: testCase.filename, apiProvider: apiProvider, tables: [])
+            XCTAssertEqual(storageCore.filename, testCase.filename)
+        }
+    }
+    
+    func testCtorDtor() throws {
+        try testCase(#function, routine: {
+            let apiProvider = SQLiteApiProviderMock()
+            apiProvider.forwardsCalls = true
+            var expectedCtorCalls = [SQLiteApiProviderMock.Call]()
+            var expectedDtorCalls = [SQLiteApiProviderMock.Call]()
+            var ctorCalls = [SQLiteApiProviderMock.Call]()
+            var dtorCalls = [SQLiteApiProviderMock.Call]()
+            var filename = ""
+            try section("file", routine: {
+                filename = "db.sqlite"
+                expectedCtorCalls = []
+                expectedDtorCalls = []
+            })
+            try section("memory", routine: {
+                try section("empty filename", routine: {
+                    filename = ""
+                })
+                try section(":memory: filename", routine: {
+                    filename = ":memory:"
+                })
+                expectedCtorCalls = [.init(id: 0, callType: .sqlite3Open(filename, .ignore))]
+                expectedDtorCalls = [.init(id: 0, callType: .sqlite3Close(.ignore))]
+            })
+            var storageCore: StorageCore? = try StorageCoreImpl(filename: filename,
+                                                                apiProvider: apiProvider,
+                                                                tables: [])
+            _ = storageCore
+            ctorCalls = apiProvider.calls
+            apiProvider.resetCalls()
+            storageCore = nil
+            dtorCalls = apiProvider.calls
+            XCTAssertEqual(expectedCtorCalls, ctorCalls)
+            XCTAssertEqual(expectedDtorCalls, dtorCalls)
+        })
+    }
+    
+    func testColumnNameWithReservedKeyword() throws {
+        struct Object: Initializable {
+            var id = 0
+            var order = 0
+        }
+        let storageCore = try StorageCoreImpl(filename: "",
+                                              tables: Table<Object>(name: "objects",
+                                                                    columns:
+                                                                        Column(name: "id", keyPath: \Object.id),
+                                                                    Column(name: "order", keyPath: \Object.order)))
+        switch storageCore.syncSchema(preserve: true) {
+        case .success(_):
+            break
+        case .failure(let error):
+            throw error
+        }
+    }
+}
